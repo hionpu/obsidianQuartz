db612cda6e4bb2b9a2d5c92cfe4add413f6cdc3d
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2024-11-29 12:04:43 +0900

    foo

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "프데달면프로데뷔함",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
