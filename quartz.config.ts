f27427d330da3c1523ca3ffa3a580dbb093dbe86
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2024-12-06 10:19:07 +0900

    foo

--- quartz.config.ts
+++ quartz.config.ts
@@ -91,6 +91,16 @@ const config: QuartzConfig = {
       Plugin.NotFoundPage(),
     ],
   },
+  defaultLanguage: 'ko',
+  languages: ['ko', 'en'],
+  i18n: {
+    ko: {
+      // 한국어 번역
+    },
+    en: {
+      // 영어 번역
+    }
+  }
 }
 
 export default config
