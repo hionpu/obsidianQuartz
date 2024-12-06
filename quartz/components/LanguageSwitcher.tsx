3ac34b4a2aef6179ed9aee40f11e64682b615cdc
박승우우 <b23038.tdc.hanmacgroup@gmail.com>
2024-12-06 10:58:38 +0900

    Revert "lang switch button"
    This reverts commit 31b399130ddcfdf20290ac49f1814600da7b56de.

--- quartz/components/LanguageSwitcher.tsx
+++ quartz/components/LanguageSwitcher.tsx
@@ -9,7 +9,7 @@ export function LanguageSwitcher({ currentLang }: LanguageSwitcherProps) {
     const currentPath = window.location.pathname
     const newPath = currentLang === 'ko' 
       ? `/en${currentPath}`
-      : currentPath.replace('/en/', '')
+      : currentPath.replace('/en', '')
     window.location.href = newPath
   }, [currentLang])
 
@@ -18,17 +18,8 @@ export function LanguageSwitcher({ currentLang }: LanguageSwitcherProps) {
       <button 
         onClick={() => switchLanguage(currentLang === 'ko' ? 'en' : 'ko')}
         className="language-button"
-        aria-label={currentLang === 'ko' ? 'Switch to English' : '한국어로 전환'}
       >
-        <img 
-          src={currentLang === 'ko' 
-            ? '/static/images/gb-flag.svg' 
-            : '/static/images/kr-flag.svg'
-          } 
-          alt={currentLang === 'ko' ? 'English' : '한국어'}
-          width="24"
-          height="24"
-        />
+        {currentLang === 'ko' ? 'English' : '한국어'}
       </button>
     </div>
   )
